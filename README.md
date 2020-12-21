c4bf8c93ae6f2349bd25f731a1af0b30a7ace11d
Claire Froelich <claire.froelich@gmail.com>
2020-12-20 20:32:03 -0800

    Play filler music in place of ad

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Shuts up annoying Spotify ads for a less jarring listening experience.
 ### Inspiration
 
 I learned a lot from this repo: https://github.com/pinavetas/SpotifyMuteAds
+
+### Filler music credits
+
+Found on [SoundClouds's freebamusic](https://soundcloud.com/freebmusic)
+
+- [Onyx - Astral](https://www.youtube.com/watch?v=nzjMYkKuyWY)
--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 const SPOTIFY_URL = "open.spotify.com/";
 const SPOTIFY_AD_TITLE = "Advertisement ·";
 
+const fillerMusic1 = new Audio("music/Astral.mp3");
+
 const titleIsAd = (title) => {
 	const adRegex = new RegExp(`^${SPOTIFY_AD_TITLE}`);
 	return !!title.match(adRegex);
@@ -14,17 +16,31 @@ const unmute = (tabId) => {
 	chrome.tabs.update(tabId, { muted: false });
 };
 
+const stop = (audio) => {
+	audio.pause();
+	audio.currentTime = 0;
+};
+
+const playFillerInNewTab = () => {
+	chrome.tabs.create({ url: "https://www.google.com/", selected: false }, function (tab) {
+		fillerMusic1.play();
+		chrome.tabs.remove(tab.id);
+	});
+};
+
 const checkForAd = (tabId, _changeInfo, tab) => {
 	if (tab.url.match(SPOTIFY_URL)) {
 		if (titleIsAd(tab.title)) {
 			console.log("Ad detected");
 			if (!tab.mutedInfo.muted) {
 				mute(tabId);
+				playFillerInNewTab();
 			}
 		} else {
 			if (tab.mutedInfo.muted) {
 				unmute(tabId);
 			}
+			stop(fillerMusic1);
 		}
 	}
 };
